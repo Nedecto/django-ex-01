7722d097bcdd5be5be089341bc5dc6f437d0c148
Gavin <nedecto@gmail.com>
2018-11-12 18:56:18 -0700

    Add files via upload

---
+++ django-ex-01/site_app/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render
+
+# generic views - see more at https://docs.djangoproject.com/en/2.1/ref/class-based-views/
+from django.views import generic
+
+from .models import Recruit, Job, RecruitJob
+
+class HomeView(generic.TemplateView):
+	template_name = 'index.html'
+
+
+class RecruitView(generic.DetailView):
+	model = Recruit
+	template_name = 'recruit.html'
+
+class RecruitListView(generic.ListView):
+	model = Recruit
+	template_name = 'recruit_list.html'
+
+class JobView(generic.DetailView):
+	model = Job 
+	template_name = 'job.html'
+
+class RecruitJobList(generic.ListView):
+	model = RecruitJob
+	template_name = 'recruit_jobs_list.html'
